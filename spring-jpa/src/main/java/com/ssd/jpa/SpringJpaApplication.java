6b875d5a58d0bb6e3e8d83bf1386e29565363fe1
rajulenka5 <rajulenka5456@gmail.com>
2022-03-17 11:45:48 +0530

    i change the connect from main class

--- spring-jpa/src/main/java/com/ssd/jpa/SpringJpaApplication.java
+++ spring-jpa/src/main/java/com/ssd/jpa/SpringJpaApplication.java
@@ -7,12 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringJpaApplication {
 
 	public static void main(String[] args) {
-		System.out.println("hi");
-		System.out.println("hi");
-		System.out.println("hi");
-		System.out.println("hi");
-		System.out.println("hi");
-		System.out.println("hi");
+	
 		SpringApplication.run(SpringJpaApplication.class, args);
 	}
 
