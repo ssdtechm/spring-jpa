f5be19b5eec794121c24ed0129537ce60cdd0575
Lalith kumar <lallugullu62@gmail.com>
2022-03-20 11:01:31 +0530

    i add new feiled in Coures

--- spring-jpa/src/main/java/com/ssd/jpa/entity/Course.java
+++ spring-jpa/src/main/java/com/ssd/jpa/entity/Course.java
@@ -29,6 +29,8 @@ public class Course {
     private Long courseId;
     private String title;
     private Integer credit;
+    
+    private Integer duration;
 
     @OneToOne(
             mappedBy = "course"
